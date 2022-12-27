9662618849993809acd9e171104357bf0e417e99
Justin Lee <joonsmoons@gmail.com>
2022-12-27 10:19:26 +0900

    [add] timer content

---
+++ 05_node/05_timer.js
@@ -0,0 +1,22 @@
+// const timeout = setTimeout(() => {
+//   console.log("1초후에 실행됩니다");
+// }, 1000);
+
+const interval = setInterval(() => {
+  console.log("1초 마다 실행됩니다");
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(interval);
+}, 3000);
+
+// clearTimeout()
+// clearImmediate()
+
+const immediate = setImmediate(() => {
+  console.log(
+    "setImmediate() 함수 호출 뒤에 오는 모든 코드를 먼저 실행하고 바로 다음에 실행이 됩니다."
+  );
+});
+
+console.log("setImmediate보다 먼저 실행이 됩니다.");
