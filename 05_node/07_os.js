841609cb4b141cf63f83d559dc0c02c6ff24c511
Justin Lee <joonsmoons@gmail.com>
2022-12-27 14:16:33 +0900

    [add] os

---
+++ 05_node/07_os.js
@@ -0,0 +1,7 @@
+const os = require("os");
+
+// console.log(os.arch()); // CPU 아키텍쳐
+// console.log(os.cpus()); // CPU 코어 정보
+// console.log(os.hostname()); // OS 호스트명
+// console.log(os.type()); // OS 타입
+console.log(os.tmpdir()); // 임시 디렉토리
